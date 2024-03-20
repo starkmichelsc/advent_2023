a994c9520091f96185ae84ca470771a8f3870a3c
Chris Michels <michelsc@starktechgroup.com>
2024-03-20 14:53:11 -0400

    add day3

---
+++ day03/__main__.py
@@ -0,0 +1,15 @@
+from day03.part01 import execute as part01
+
+from day03.part02 import execute as part02
+
+full_data = "data/full.txt"
+
+
+def main():
+
+    print(f"part 1: {part01(full_data)}")
+    # print(f"part 2: {part02(full_data)}")
+
+
+if __name__ == "__main__":
+    main()
